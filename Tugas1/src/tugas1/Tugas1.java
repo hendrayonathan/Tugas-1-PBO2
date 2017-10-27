2e245ee547b8194a12d711598cf65cc739019f26
hendrayonathan <32958616+hendrayonathan@users.noreply.github.com>
2017-10-27 17:48:40 +0800

    menjalankan method pada objek m

--- Tugas1/src/tugas1/Tugas1.java
+++ Tugas1/src/tugas1/Tugas1.java
@@ -7,6 +7,12 @@ package tugas1;
 public class Tugas1 {
     public static void main(String[] args) {
         myFrame m = new myFrame();
+        
+        m.pesan = "Halo...";
+        m.bilBulat = 5;
+        m.bilDesimal =3.14;
+        
+        m.cetakAttribute();
     }
     
 }
