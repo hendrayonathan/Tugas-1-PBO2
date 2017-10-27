5c499b15bd42b5f7d26e15155b860cda99b13511
hendrayonathan <32958616+hendrayonathan@users.noreply.github.com>
2017-10-27 17:31:42 +0800

    menambahkan "attribute"

---
+++ Tugas1/src/tugas1/myFrame.java
@@ -0,0 +1,11 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas1;
+public class myFrame {
+    string pesan;
+    int bilBulat;
+    double bilDesimal;
+}
